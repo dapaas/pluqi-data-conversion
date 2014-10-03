cf2a390f66fa917205f78299d683964d7617a9b8
Rick Moynihan <rick.m@swirrl.com>
2014-10-03 11:02:26 +0100

    Grafter template

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject pluqi-transformation "0.1.0-SNAPSHOT"
+  :description "FIXME: this part is for you!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [grafter "0.2.0-SNAPSHOT"]
+                 [org.slf4j/slf4j-jdk14 "1.7.5"]]
+  :main pluqi-transformation.core
+  :aot [pluqi-transformation.core])
---
+++ src/pluqi_transformation/core.clj
@@ -0,0 +1,28 @@
+(ns pluqi-transformation.core
+  (:require [grafter.tabular :refer :all]
+            [grafter.rdf :refer :all]
+            [grafter.rdf.sesame :as ses]
+            [pluqi-transformation.graph :refer [make-graph]]
+            [pluqi-transformation.pipeline :refer [pipeline]])
+  (:gen-class))
+
+(defn import-data
+  [quads-seq destination]
+  (let [quads (->> quads-seq
+                   ;;(filter remove-invalid-triples)
+                   )]
+
+    (add (ses/rdf-serializer destination) quads)))
+
+(defn -main [& [path output]]
+  (when-not (and path output)
+    (println "Usage: lein run <input-file.csv> <output-file.(nt|rdf|n3|ttl)>")
+    (System/exit 0))
+
+  (-> (open-all-datasets path)
+      first
+      pipeline
+      make-graph
+      (import-data output))
+
+  (println path "=>" output))
